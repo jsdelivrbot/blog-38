ede6502314bd35f4363cbfd13f63cc9ec043f82b
Andres Vizcaino <andruvizcaino1989@gmail.com>
2017-05-11 01:13:29 -0500

    rendering list of posts

--- src/reducers/reducers_posts.js
+++ src/reducers/reducers_posts.js
@@ -1,9 +1,11 @@
+import _ from 'lodash';
 import { FETCH_POSTS } from '../actions';
 
 export default function(state = {}, action) {
   switch (action.type) {
     case FETCH_POSTS:
-      console.log(action.payload.data);
+      return _.mapKeys(action.payload.data, 'id');
+      // Gets an array and creates and object => {"4":{"id":4,"title":"hi"},"36":{"id":36,"title":"hey"}}
     default:
       return state;
   }
